e95b49906c94f8bb0914c8003c8b3eb94fb4c6bf
Vincenzo Tramo <vv.tramo@gmail.com>
2023-08-28 19:35:28 +0200

    feat(iotsecurity): add ErrorCodes

---
+++ iot-security/src/main/java/com/sensorwave/iotsecurity/exhandlers/ErrorCodes.java
@@ -0,0 +1,9 @@
+package com.sensorwave.iotsecurity.exhandlers;
+
+public class ErrorCodes {
+
+    public static final String KC_USER_CREATION_DUP_USERNAME_ERROR_CODE = "iotsecurity-kc-user-001";
+    public static final String KC_USER_CREATION_UNAUTHORIZED_ERROR_CODE = "iotsecurity-kc-user-002";
+    public static final String KC_ADMIN_TOKEN_CREATION_ERROR_CODE = "iotsecurity-kc-admin-001";
+    public static final String INTERNAL_SERVER_ERROR_CODE = "iotsecurity-server-error";
+}
