74a7dc68ca30a998cc7e4bb81e18865ab7fe5423
Vincenzo Tramo <vv.tramo@gmail.com>
2023-08-28 19:35:34 +0200

    feat(iotprocessor): add ErrorCodes

---
+++ iot-processor/src/main/java/com/sensorwave/iotprocessor/exhandlers/ErrorCodes.java
@@ -0,0 +1,6 @@
+package com.sensorwave.iotprocessor.exhandlers;
+
+public class ErrorCodes {
+    public static final String ROOM_CREATION_DUP_NAME_ERROR_CODE = "iotprocessor-room-001";
+    public static final String INTERNAL_SERVER_ERROR_CODE = "iotprocessor-server-error";
+}
