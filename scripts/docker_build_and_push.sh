47d13c746a2967af6c71c102625bac55fefe491c
Vincenzo Tramo <vv.tramo@gmail.com>
2023-08-28 19:42:03 +0200

    ops: add docker build and push script

---
+++ scripts/docker_build_and_push.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+registry_url=${REGISTRY_URL:-localhost:5000}
+
+read_image_tag() {
+	local image_name=$1
+	echo -n "$image_name TAG: "
+	read -r image_tag
+	image_tag="$image_tag"
+}
+
+ask_confirm_push() {
+	local image_name=$1
+	full_image_name="$registry_url/sensorwave/$image_name:$image_tag"
+  echo -n "I am going to push the image $full_image_name. Proceed? [y/n]: "
+  read -r ans
+	[ "$ans" = "y" ] && return 0 || return 1
+}
+
+docker_build_and_push() {
+	local image_name=$1
+
+	docker buildx build \
+    --push --quiet -t "$registry_url/sensorwave/$image_name:$image_tag" \
+    -f "../$image_name/src/main/docker/Dockerfile.jvm" "../$image_name" \
+		&& echo -e "Image $full_image_name pushed."
+}
+
+
+services=(${*:-iot-processor iot-security geocoder})
+
+for image_name in "${services[@]}"; do
+  read_image_tag "$image_name"
+
+  ask_confirm_push "$image_name" && \
+    docker_build_and_push "$image_name" || echo -e "Cancelled."
+
+  echo -e "\n"
+done			
+
+exit 0
